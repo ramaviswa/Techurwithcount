a9174113bcd5322e6f544287b89dd8fb6f340387
jagadish R <viswa4323@gmail.com>
2022-05-27 12:53:41 +0530

    resource

--- infra/resource/resourcegroup/rg.tf
+++ infra/resource/resourcegroup/rg.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resource" {
     count = var.rg.count
-    name = var.rg.name[count.index]
+    name = "${var.rg.name}-${count.index}"
     location = var.location 
 }
