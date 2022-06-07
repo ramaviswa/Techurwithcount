2c668107cc442e6f9b3de97b9c4c352bd5a3a2f9
jagadish R <viswa4323@gmail.com>
2022-06-07 14:46:59 +0530

    Azure AD

--- infra/resource/azad/adusers.tf
+++ infra/resource/azad/adusers.tf
@@ -10,11 +10,18 @@ data "azuread_domains" "techurtenetdomain" {
   only_initial = true
 }
 
+resource "azuread_application" "appregistration" {
+  display_name = "techurappregistration"
+}
+
+resource "azuread_service_principal" "serviceprincipal" {
+  application_id = azuread_application.appregistration.id
+}
 
 
-resource "azuread_user" "user" {
+/* resource "azuread_user" "user" {
     user_principal_name = "${var.user.principalname}@${data.azuread_domains.techurtenetdomain.domains.0.domain_name}"
     display_name = var.user.name
     mail_nickname = var.user.nickname
     password = var.user.password  
-}
+}*/
