dd90d13f46d7e7f9ef3d004a110115700896ce5f
jagadish R <viswa4323@gmail.com>
2022-06-08 14:08:51 +0530

    azkeyvault

--- infra/resource/keyvault/keyvault.tf
+++ infra/resource/keyvault/keyvault.tf
@@ -12,7 +12,7 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "keyvault" {
     name = var.keyvault.name
     location = var.rg[var.keyvault.rgkey].location
-    resource_group_name = var.rg[var.keyvault.rgkey].location
+    resource_group_name = var.rg[var.keyvault.rgkey].name
     enabled_for_disk_encryption = true
     tenant_id = data.azurerm_client_config.current.tenant_id
     soft_delete_retention_days = 7
